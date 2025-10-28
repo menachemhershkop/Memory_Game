ac34b5cbfb73b5bed1ba0c49ff952f219821ce20
mendy hershkop <hers.mm@gmail.com>
2025-10-28 11:53:08 +0200

    create cards and swap them

--- logic/cards.py
+++ logic/cards.py
@@ -1,1 +1,14 @@
-ECHO is on.
+import random
+def create_cards(num_of_cards:int=5):
+    ALPHABET_LIST = [chr(i) for i in range(65, 90)]
+    cuple_cards=[]
+    for i in range(num_of_cards):
+        cuple_cards.append(ALPHABET_LIST[i])
+        cuple_cards.append(ALPHABET_LIST[i])
+    return cuple_cards
+def swap_cards(card_list):
+    for i in range(1000):
+        card_1=random.randint(0,len(card_list)-1)
+        card_2=random.randint(0,len(card_list)-1)
+        card_list[card_1], card_list[card_2] = card_list[card_2], card_list[card_1]
+    return card_list
--- main.py
+++ main.py
@@ -1,1 +1,4 @@
-ECHO is on.
+from logic.cards import create_cards, swap_cards
+
+if __name__=="__main__":
+    print(swap_cards(create_cards()))
