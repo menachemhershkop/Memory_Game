737253aaa88e8d63d61d05f78cc42ce175beb8e6
mendy hershkop <hers.mm@gmail.com>
2025-10-28 11:24:25 +0200

    build the cards board

--- logic/board.py
+++ logic/board.py
@@ -1,3 +1,4 @@
+
 def creat_board(size : int = 4):
     board=[]
     for i in range(1,size+1):
@@ -6,3 +7,15 @@ def creat_board(size : int = 4):
             row.append("X")
         board.append(row)
     return board
+def creat_card_boards(cards : list[str], board):
+    game_board=[]
+    for i in range(1,len(cards)+1):
+        row=[]
+        for j in range(1,len(cards)+1):
+            row.append(j)
+        game_board.append(row)
+    if len(game_board)== len(board):
+        return game_board
+    else:
+        return "incorrect board"
+print(creat_card_boards(["1","2","3","4"],creat_board()))
